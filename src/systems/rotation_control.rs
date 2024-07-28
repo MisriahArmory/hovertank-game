c0cd54c952cb55c9b80d4ea7a12f03d925227726
ChosunOne <chosunone@protonmail.com>
2024-07-28 17:42:01 +0900

    refactor: reduce nesting in rotation system

--- src/systems/rotation_control.rs
+++ src/systems/rotation_control.rs
@@ -17,46 +17,47 @@ pub fn rotation_control(
     let action = control_query.single();
     let mut camera_transform = camera_query.single_mut();
 
-    if let Some(ax_data) = action.axis_pair(&RotationAction::Rotate) {
-        let yaw_rotor = match (ax_data.x().abs(), ax_data.x().signum()) {
-            (0.0, 1.0) => Quat::IDENTITY,
-            (_, -1.0) => Quat::from_rotation_arc(Vec3::Z, Vec3::X),
-            (_, 1.0) => Quat::from_rotation_arc(Vec3::X, Vec3::Z),
-            _ => Quat::IDENTITY,
-        };
+    let Some(ax_data) = action.axis_pair(&RotationAction::Rotate) else {
+        return;
+    };
+    let yaw_rotor = match (ax_data.x().abs(), ax_data.x().signum()) {
+        (0.0, 1.0) => Quat::IDENTITY,
+        (_, -1.0) => Quat::from_rotation_arc(Vec3::Z, Vec3::X),
+        (_, 1.0) => Quat::from_rotation_arc(Vec3::X, Vec3::Z),
+        _ => Quat::IDENTITY,
+    };
 
-        let yaw_speed = (ax_data.x().abs() * CAMERA_YAW_SPEED).min(CAMERA_MAX_YAW_SPEED);
-        camera_transform.rotation = camera_transform
-            .rotation
-            .slerp(
-                yaw_rotor * camera_transform.rotation,
-                yaw_speed * time.delta_seconds(),
-            )
-            .normalize();
+    let yaw_speed = (ax_data.x().abs() * CAMERA_YAW_SPEED).min(CAMERA_MAX_YAW_SPEED);
+    camera_transform.rotation = camera_transform
+        .rotation
+        .slerp(
+            yaw_rotor * camera_transform.rotation,
+            yaw_speed * time.delta_seconds(),
+        )
+        .normalize();
 
-        let forward = camera_transform.forward().into();
-        let up = camera_transform.up().into();
-        let pitch_rotor = match (ax_data.y().abs(), ax_data.y().signum()) {
-            (0.0, 1.0) => Quat::IDENTITY,
-            (_, -1.0) => Quat::from_rotation_arc(forward, up),
-            (_, 1.0) => Quat::from_rotation_arc(up, forward),
-            _ => Quat::IDENTITY,
-        };
+    let forward = camera_transform.forward().into();
+    let up = camera_transform.up().into();
+    let pitch_rotor = match (ax_data.y().abs(), ax_data.y().signum()) {
+        (0.0, 1.0) => Quat::IDENTITY,
+        (_, -1.0) => Quat::from_rotation_arc(forward, up),
+        (_, 1.0) => Quat::from_rotation_arc(up, forward),
+        _ => Quat::IDENTITY,
+    };
 
-        let pitch_target = pitch_rotor * camera_transform.rotation;
-        let pitch_speed = (ax_data.y().abs() * CAMERA_PITCH_SPEED).min(CAMERA_MAX_PITCH_SPEED);
-        let new_rotation = camera_transform
-            .rotation
-            .slerp(pitch_target, pitch_speed * time.delta_seconds())
-            .normalize();
-        let rotation_target = camera_transform.with_rotation(new_rotation);
+    let pitch_target = pitch_rotor * camera_transform.rotation;
+    let pitch_speed = (ax_data.y().abs() * CAMERA_PITCH_SPEED).min(CAMERA_MAX_PITCH_SPEED);
+    let new_rotation = camera_transform
+        .rotation
+        .slerp(pitch_target, pitch_speed * time.delta_seconds())
+        .normalize();
+    let rotation_target = camera_transform.with_rotation(new_rotation);
 
-        let forward = rotation_target.forward();
-        let forward_xz = Vec3::new(forward.x, 0.0, forward.z);
-        let pitch = forward.angle_between(forward_xz);
+    let forward = rotation_target.forward();
+    let forward_xz = Vec3::new(forward.x, 0.0, forward.z);
+    let pitch = forward.angle_between(forward_xz);
 
-        if (CAMERA_MIN_PITCH..=CAMERA_MAX_PITCH).contains(&pitch) {
-            camera_transform.rotation = rotation_target.rotation;
-        }
+    if (CAMERA_MIN_PITCH..=CAMERA_MAX_PITCH).contains(&pitch) {
+        camera_transform.rotation = rotation_target.rotation;
     }
 }
